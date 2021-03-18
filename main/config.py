dc99ac9c7f2ca8ec559f017fcee89c64c046e95e
JaddaHub <56644672+JaddaHub@users.noreply.github.com>
2021-03-18 22:06:38 +0500

    huy

--- main/config.py
+++ main/config.py
@@ -1,5 +1,5 @@
 TOKEN = "b58aff9b5581f0290bd7bdcab84ecccc94d0814851e8b2d33a11889843ed7d3249e14eb2eda430088da05"
 group_id = 202934978
-login_mongo = "zerochi"
-pass_mongo = "muvuskezdu197"
-a = 1
+login_mongo = "maxim"
+pass_mongo = "uDLAZ0drJF00kx48"
+kolya = 'loh'
