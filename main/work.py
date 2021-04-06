5abcf98e0352f66c2728710c72f7af256e287423
k33pup <n.bog05@mail.ru>
2021-04-06 21:03:15 +0500

    тест

--- main/work.py
+++ main/work.py
@@ -8,8 +8,13 @@ class Work:
         if collection.count_documents({"_id": user_id}) == 0:
             vk_session.method('messages.send',
                               {'chat_id': id_chat, 'message': f"У вас нет бандита!",
-                               'random_id': 0, "keyboard": general_keyboard})
-        elif collection.find_one({"_id": user_id})["Работа"]["Закончил работу"] == 0:
+                               'random_id': 0, "keyboard": None})
+        elif collection.find_one({"_id": user_id})["Начал работу"] != 0:
+            vk_session.method('messages.send',
+                              {'chat_id': id_chat,
+                               'message': f"Вы уже на работе!",
+                               'random_id': 0})
+        elif collection.find_one({"_id": user_id})["Начал работу"] == 0:
             collection.update_one({"_id": user_id}, {"$set": {"Начал работу": start_date}})
             vk_session.method('messages.send',
                               {'chat_id': id_chat,
@@ -18,23 +23,23 @@ class Work:
 
     def stop_work(self, event, collection, id_chat, vk_session):
         user_id = event.object.message['from_id']
-        start_date = collection.find_one({"_id": user_id})["Работа"]["Начал работу"]
+        start_date = collection.find_one({"_id": user_id})["Начал работу"]
         now_date = datetime.datetime.today()
         if collection.count_documents({"_id": user_id}) == 0:
             vk_session.method('messages.send',
                               {'chat_id': id_chat, 'message': f"У вас нет бандита!",
-                               'random_id': 0, "keyboard": general_keyboard})
+                               'random_id': 0, "keyboard": None})
         elif start_date == 0:
             vk_session.method('messages.send',
                               {'chat_id': id_chat, 'message': f"Вы не выходили на работу!",
-                               'random_id': 0, "keyboard": general_keyboard})
+                               'random_id': 0, "keyboard": None})
         elif start_date + datetime.timedelta(minutes=5) <= now_date:
-            collection.update_one({"_id": user_id}["Работа"], {"$set": {"Начал работу": 0}})
-            collection.update_one({"_id": user_id}["Работа"], {"$set": {"Закончил работу": now_date}})
+            collection.update_one({"_id": user_id}, {"$set": {"Начал работу": 0}})
+            collection.update_one({"_id": user_id}, {"$set": {"Закончил работу": now_date}})
             vk_session.method('messages.send',
                               {'chat_id': id_chat, 'message': f"Вы поработали хорошо!",
-                               'random_id': 0, "keyboard": general_keyboard})
+                               'random_id': 0, "keyboard": None})
         else:
             vk_session.method('messages.send',
                               {'chat_id': id_chat, 'message': f"Еще рано уходить!",
-                               'random_id': 0, "keyboard": general_keyboard})
+                               'random_id': 0, "keyboard": None})
